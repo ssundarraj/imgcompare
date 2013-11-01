0051671003e2f1742afe908cc4da7399c592735b
Sriram Sundarraj <sriram.s.1994@gmail.com>
2013-11-02 00:07:37 +0530

    Added a fourth function that seems to compare correctly.

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a python script that checks how different or similar two images are. 
+
+Execute:
+
+python imagecompare.py image1.format image2.format
+
+
--- imgcompare.py
+++ imgcompare.py
@@ -45,12 +45,29 @@ def mydiff3(im1, im2):
 
 def splitdiff(im1, im2):
 	nbox=20
-	w1=im1.size[0]/nbox
-	h1=im1.size[1]/nbox
-	w2=im2.size[0]/nbox
-	h2=im2.size[1]/nbox
-	#for i in range():
-	return nbox
+	width1=im1.size[0]/nbox
+	height1=im1.size[1]/nbox
+	width2=im2.size[0]/nbox
+	height2=im2.size[1]/nbox
+	p1=im1.load()
+	p2=im2.load()
+	il1=[0]*(nbox**2)#stores the sum of the RGB values for each box
+	il2=[0]*(nbox**2)
+	rms=0
+	for x in range(nbox):
+		for y in range(nbox):
+			for i in range(width1):
+				for j in range(height1):
+					il1[nbox*x+y]=il1[nbox*x+y]+(sum(p1[width1*y+i, height1*x+j]))
+			il1[nbox*x+y]=il1[nbox*x+y]/(height1*width1)
+			for i in range(width2):
+				for j in range(height2):
+					il2[nbox*x+y]=il2[nbox*x+y]+(sum(p2[width2*y+i, height2*x+j]))
+			il2[nbox*x+y]=il2[nbox*x+y]/(height2*width2)
+	for i in range(min(len(il1), len(il2))):
+		rms+=il1[i]**2-il2[i]**2
+	rms=math.sqrt(math.fabs(rms/min(len(il1), len(il2))))
+	return rms
 
 if __name__=="__main__":
 	img1=Image.open(sys.argv[1])
